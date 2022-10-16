d304eafb7072a466ec79eb0045e3809483038035
FrankAlbella <frankalbella@gmail.com>
2022-10-16 14:14:49 -0400

    Add veggie and meat topping options

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
   const [selected, setSelected] = useState([]);
-  const toppings = ["Pepperoni", "Sausage", "Bacon", "Peppers", "Onions", "Mushrooms"];
+  const meat_toppings = ["Pepperoni", "Sausage", "Chicken", "Sausage"];
+  const veggie_toppings = ["Peppers", "Onions", "Pickle", "Spinach"];
+  const other_toppings = ["Extra cheese", "Extra oil", "Extra sauce", "Hot sauce", "Mayonnaise", "Mustard"];
+
 
   const handleSelect = (event) => {
     var updatedToppings = [...selected];
-    if (event.target.selected){
-      updatedToppings = [...selected,event.target.value];
+    if (event.target.checked) {
+      updatedToppings = [...selected, event.target.value];
     } else {
       updatedToppings.splice(selected.indexOf(event.target.value), 1);
     }
@@ -16,20 +19,42 @@ function App() {
   };
 
   const selectedItems = selected.length
-    ? selected.reduce((total,item) => {
+    ? selected.reduce((total, item) => {
       return total + "' " + item;
-      })
+    })
     : "";
 
-    var isSelected = (item) =>
+  var isSelected = (item) =>
     selected.includes(item) ? "selected-item" : "not-selected-item";
-    
+
   return (
     <div className="app">
-      <div className="toppings">
-        <div className="title">Your Toppings:</div>
+      <div className="meat_toppings">
+        <div className="title">Meat Toppings:</div>
         <div className="list-container">
-          {toppings.map((item,index) => (
+          {meat_toppings.map((item, index) => (
+            <div key={index}>
+              <input value={item} type="checkbox" onChange={handleSelect} />
+              <span className={isSelected(item)}>{item}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="veggie_toppings">
+        <div className="title">Veggie Toppings:</div>
+        <div className="list-container">
+          {veggie_toppings.map((item, index) => (
+            <div key={index}>
+              <input value={item} type="checkbox" onChange={handleSelect} />
+              <span className={isSelected(item)}>{item}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="other_toppings">
+        <div className="title">Other Toppings:</div>
+        <div className="list-container">
+          {other_toppings.map((item, index) => (
             <div key={index}>
               <input value={item} type="checkbox" onChange={handleSelect} />
               <span className={isSelected(item)}>{item}</span>
@@ -38,7 +63,7 @@ function App() {
         </div>
       </div>
       <div>
-        {'Items checked are:'}{selectedItems}
+        {'Items checked are: '}{selectedItems}
       </div>
     </div>
   );
