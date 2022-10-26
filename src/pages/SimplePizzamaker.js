c981e5d0883035a6929762ae4e3fabadcfbeb3bf
corygrossman <cory.grossman@ufl.edu>
2022-10-26 16:32:12 -0400

    Changed Design 1 page and added assets

--- src/pages/SimplePizzamaker.js
+++ src/pages/SimplePizzamaker.js
@@ -4,9 +4,9 @@ import '../App.css';
 
 function SimplePizzamaker() {
   const [selected, setSelected] = useState([]);
-  const meat_toppings = ["Pepperoni", "Bacon", "Chicken", "Sausage"];
-  const veggie_toppings = ["Peppers", "Onions", "Pickle", "Spinach"];
-  const other_toppings = ["Extra cheese", "Extra oil", "Extra sauce", "Hot sauce", "Mayonnaise", "Mustard"];
+  const meat_toppings = ["Pepperoni", "Bacon", "Sausage"];
+  const veggie_toppings = ["Peppers", "Onions", "Spinach"];
+  const other_toppings = ["Extra cheese", "Olive Oil", "Hot sauce"];
 
 
   const handleSelect = (event) => {
@@ -26,21 +26,24 @@ function SimplePizzamaker() {
     <div className="app">
       <div className="main-content">
         <div className="pizzaContainer">
-        <div className="header">Your Pizza</div>
-        <div className="pizza-overview">
-          <img src="/images/pizza-background.png" alt="Pizza background"></img>
-          {selected.map((item, index) => (
-            <img key={index} src={"/images/" + item + ".png"} alt={item} />
-          ))}
-        </div>
+          <div className="fixed-pizza">
+            <div className="header">Your Pizza</div>
+            <div className="pizza-overview">
+              <img src="/images/design1/pizza-background.png" alt="Pizza background"></img>
+              {selected.map((item, index) => (
+                <img key={index} src={"/images/design1/" + item + ".png"} alt={item} />
+              ))}
+            </div>
+          </div>
+          <hr />
         </div>
         <div className="options">
-        <div className="header">Choose Your Toppings</div>
+        <div className="header2">Choose Your Toppings</div>
         <div className="toppings-box">
           <div className="title">Meat Toppings</div>
           <div className="list-container small-margin">
             {meat_toppings.map((item, index) => (
-              <div className="checkbox-rect" key={index}>
+              <div className="checkbox-rect2" key={index}>
                 <input id={item + "-id"} value={item} type="checkbox" onChange={handleSelect} />
                 <label htmlFor={item + "-id"} className={isSelected(item)} >{item}</label>
               </div>
@@ -51,7 +54,7 @@ function SimplePizzamaker() {
           <div className="title">Veggie Toppings</div>
           <div className="list-container small-margin">
             {veggie_toppings.map((item, index) => (
-              <div className="checkbox-rect" key={index}>
+              <div className="checkbox-rect2" key={index}>
                 <input id={item + "-id"} value={item} type="checkbox" onChange={handleSelect} />
                 <label htmlFor={item + "-id"} className={isSelected(item)}>{item}</label>
               </div>
@@ -62,7 +65,7 @@ function SimplePizzamaker() {
           <div className="title">Other Toppings</div>
           <div className="list-container small-margin">
             {other_toppings.map((item, index) => (
-              <div className="checkbox-rect" key={index}>
+              <div className="checkbox-rect2" key={index}>
                 <input id={item + "-id"} value={item} type="checkbox" onChange={handleSelect} />
                 <label htmlFor={item + "-id"} className={isSelected(item)}>{item}</label>
               </div>
