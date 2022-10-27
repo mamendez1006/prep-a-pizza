d6e8712e1078cfdd03e1e9002027fc154645701d
corygrossman <cory.grossman@ufl.edu>
2022-10-27 10:01:13 -0400

    Updating

---
+++ src/pages/Design1.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import { Link } from "react-router-dom";
+
+const Home = () => (
+  <div className="app">
+    <div className="main-content-homepage">
+      <div className='homepage-image'></div>
+      <Link to="simplepizzamaker">
+          <div className="button-placement-landing">
+            <button className="button-35">Customize & Order Pizza</button>
+          </div>
+      </Link>
+    </div>
+  </div>
+);
+
+export default Home;
